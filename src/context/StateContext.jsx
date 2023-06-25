4118f10faeccc118a2d288d4ac4d9eb895935271
chanmyae-aung <chanmyaeaung.cmg@gmail.com>
2023-06-25 21:22:18 +0630

    navbar added

--- src/context/StateContext.jsx
+++ src/context/StateContext.jsx
@@ -3,9 +3,12 @@ import { createContext, useContext, useState } from "react";
 export const stateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-    
   const [show, setShow] = useState(false);
   const [showLanguage, setShowLanguage] = useState(false);
+  const [showApps, setShowApps] = useState(false);
+  const [image, setImage] = useState();
+  const [showNoti, setShowNoti] = useState(false);
+  const [showSetting, setShowSetting] = useState(false)
 
   const toggleShow = () => {
     setShow(!show);
@@ -15,12 +18,41 @@ export const StateContextProvider = ({ children }) => {
     setShowLanguage(!showLanguage);
   };
 
+  const toggleApps = () => {
+    setShowApps(!showApps);
+  };
+
+  const toggleNoti = () => {
+    setShowNoti(!showNoti);
+  };
+
+  const toggleSetting = () => {
+    setShowSetting(!showSetting)
+  }
+
   const hide = () => {
     show === true && setShow(false);
     showLanguage === true && setShowLanguage(false);
+    showApps === true && setShowApps(false);
+    showNoti === true && setShowNoti(false);
+    showSetting === true && setShowSetting(false);
   };
-  
-  const data = { show, toggleShow, toggleLanguage, showLanguage, hide };
+
+  const data = {
+    show,
+    toggleShow,
+    showLanguage,
+    toggleLanguage,
+    image,
+    setImage,
+    showApps,
+    toggleApps,
+    showNoti,
+    toggleNoti,
+    showSetting,
+    toggleSetting,
+    hide,
+  };
   return <stateContext.Provider value={data}>{children}</stateContext.Provider>;
 };
 
