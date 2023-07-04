300bab5ab339a85eaacd0e03d280548afd5c589e
chanmyae-aung <chanmyaeaung.cmg@gmail.com>
2023-07-04 17:19:59 +0630

    auth pages added

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,89 @@
+import {
+    FormControl,
+    FormHelperText,
+    FormLabel,
+    Input,
+    StyledEngineProvider,
+  } from "@mui/joy";
+  import React from "react";
+  import { Link } from "react-scroll";
+  
+  export default function SignIn() {
+    const inputStyle = {
+      "--Input-focusedThickness": "0.08rem",
+      "--Input-focusedHighlight": "#00bac7",
+    };
+    return (
+      <StyledEngineProvider injectFirst>
+        <div className="w-full flex bg-[#F1F5F7] h-screen">
+          <div className="md:basis-1/2 mx-auto px-5 lg:px-[58px]">
+            <img
+              className="mb-20 sm:mb-5 lg:mb-32 py-7"
+              src="https://d33wubrfki0l68.cloudfront.net/ba6b91b7d508187d153e48318c22d0773a9cedfc/36afa/assets/images/logo.svg"
+              alt=""
+            />
+            <div className="mb-10">
+              <h2 className="text-2xl font-bold mb-2">Sign In</h2>
+              <p className="text-slate-400 text-sm">
+              Enter your email address and password to access admin panel
+              </p>
+            </div>
+            <form>
+              <div className="flex flex-col gap-5">
+                <div className="flex flex-col lg:flex-row gap-5 lg:gap-10">
+                  <div className="w-full">
+                    <FormLabel className="text-xs font-bold">
+                      Email Address
+                    </FormLabel>
+                    <Input
+                      sx={inputStyle}
+                      type="email"
+                      className="text-xs bg-[#F8FAFB]"
+                      placeholder="Your email address"
+                    />
+                  </div>
+                  <div className="w-full">
+                    <FormLabel className="text-xs font-bold">
+                      Your Password
+                    </FormLabel>
+                    <Input
+                      sx={inputStyle}
+                      type="password"
+                      className="text-xs bg-[#F8FAFB]"
+                      placeholder="Your password"
+                    />
+                  </div>
+                </div>
+                  
+              </div>
+              <div className="flex items-center mt-5 gap-3">
+                <input id="checkbox" type="checkbox" />
+                <label
+                  htmlFor="checkbox"
+                  className="text-[0.8rem] accent-cyan-500 text-slate-800 font-semibold"
+                >Remember me
+                </label>
+              </div>
+              <button className="px-7 mt-8 py-2.5 bg-[#00bac7] rounded-full text-sm text-white font-semibold w-fit">
+                Sign In
+              </button>
+            </form>
+            <div className="flex items-center gap-2 lg:absolute bottom-0 lg:py-10 py-9">
+              <p className="text-sm text-slate-400">Don't have an account yet?</p>
+              <Link to="/login" className="text-[#00bac7] text-sm font-bold cursor-pointer">
+                Sign Up
+              </Link>
+            </div>
+          </div>
+          <div className="basis-1/2 hidden lg:block h-screen brightness-50">
+            <img
+              className="w-full h-screen object-cover"
+              src="https://dashly-theme.com/assets/images/covers/sign-in-cover.jpeg"
+              alt=""
+            />
+          </div>
+        </div>
+      </StyledEngineProvider>
+    );
+  }
+  
